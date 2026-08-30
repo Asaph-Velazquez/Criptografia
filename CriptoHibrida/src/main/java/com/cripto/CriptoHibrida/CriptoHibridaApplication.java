87017d258dc18b99cfa345bc21bcf602a417b4ab
Asaph Velazquez <saulvelpa120@gmail.com>
2026-08-30 17:21:47 -0600

    chore(CriptoHibrida): se añadieron los archivos del proyecto de cripto hibrida

---
+++ CriptoHibrida/src/main/java/com/cripto/CriptoHibrida/CriptoHibridaApplication.java
@@ -0,0 +1,13 @@
+package com.cripto.CriptoHibrida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoHibridaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoHibridaApplication.class, args);
+	}
+
+}
